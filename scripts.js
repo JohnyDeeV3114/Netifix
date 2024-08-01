5a2f61ec0ec4937276d071209baf6cc6ac5abdad
seu-username <seu-email>
2024-08-01 05:52:36 -0300

    NetFlix - primeiro Commit

---
+++ scripts.js
@@ -0,0 +1,18 @@
+let botao = document.getElementById("botão-pergunta")
+let menu = document.getElementById("pergunta")
+let imagem = document.getElementById("imagem-pergunta")
+
+function abacate() {
+    if (menu.style.display == "block"){        
+        menu.style.display = "none"
+        imagem.id = "imagem-pergunta"
+    } else {
+        menu.style.display = "block"
+        imagem.id = "imagem-pergunta-girar"
+    }
+
+}
+
+botao.addEventListener("click", abacate)
+
+
